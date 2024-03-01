b5c110c8557ebd1200eca9901565e5ae6b3df549
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-03-01 23:41:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,6 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡JOIN MAIN CHANNEL⚡", callback_data = "t.me/megafilezip"),
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
