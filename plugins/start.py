7fb4640a5158fc23e53244cb6d8b324de3864478
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-03-02 20:22:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,6 +88,10 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
+                    InlineKeyboardButton("⚡ POSTING CHANNEL ⚡", url = "https://t.me/+QM4cOFy49DpkZGY9")
+                ]
+                 
+                [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
