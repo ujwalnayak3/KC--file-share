8d07a5d7f4474dbe213e42d921773f224dd80cd0
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-04-09 05:55:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Owner : @imaginebreakerr \n 👨‍💻 DEVLOPER : @ifeelscam</b>",
+            text = f"<b>👨‍💻 DEVLOPER : @ifeelscam</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
