36756724ddda101d3bf2dd469c5da4b3e072e740
D4RK ‼️ <116873243+atiiif@users.noreply.github.com>
2024-04-25 14:20:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "<b>Join My Posting Channel \n👉 @Inkalinks 👈 \nfor more videos Links!!<b>"
+USER_REPLY_TEXT = "<b>Join My Update Channel \n👉 @Publicfille 👈 \nfor more useful bots!!<b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
