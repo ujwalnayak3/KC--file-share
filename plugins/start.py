a14c22537bd19d423ddcd877f093221ccba49ee7
folieds <yosnkooke@gmail.com>
2024-09-03 10:15:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -70,7 +70,9 @@ async def start_command(client: Client, message: Message):
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
+            if DISABLE_CHANNEL_BUTTON:
             reply_markup = msg.reply_markup
+            
             else:
                 reply_markup = None
 
