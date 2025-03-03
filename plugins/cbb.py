fccabd269e16a3b26ae570060619d6a95e1f55ea
Ben 10 <uncle@flash.co>
2025-03-03 21:58:52 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -63,7 +63,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             pass
     elif data == "home":
         await query.message.edit_text(
-            text=f"<b><blockquote>👋 Hey {query.from_user.mention}\n\nI'm advance bot of providing videos for @InkaLinks Channel!!..</blockquote></b>",
+            text=f"<b><blockquote>ʜᴇʏ !! {query.from_user.mention}\n\nɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>",
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                 [
