3ed3bcc6c573a2a3b9c54f0f79327a097cfbd1a6
Ben 10 <uncle@flash.co>
2025-03-06 17:50:12 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helper_func import get_readable_time
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
