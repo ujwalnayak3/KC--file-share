49e95141ce841f754b346bba071a199ffd0960e2
Ben 10 <uncle@flash.co>
2025-03-06 17:07:29 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,12 +5,24 @@ from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
-@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
-async def stats(bot: Bot, message: Message):
+@Bot.on_message(filters.command('status') & filters.private & is_admin)
+async def info(client: Bot, message: Message):   
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("★ Cʟᴏsᴇ ★", callback_data = "close")]])
+    
+    start_time = time.time()
+    temp_msg = await message.reply("<b><i>Pʀᴏᴄᴇssɪɴɢ....</i></b>", quote=True)  # Temporary message
+    end_time = time.time()
+    
+    # Calculate ping time in milliseconds
+    ping_time = (end_time - start_time) * 1000
+    
+    users = await full_userbase()
     now = datetime.now()
     delta = now - bot.uptime
-    time = get_readable_time(delta.seconds)
-    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+    bottime = get_readable_time(delta.seconds)
+    
+    await temp_msg.edit(f"🫂 : <b>{len(users)} USERS\n\n🚀 UPTIME » {uptime}\n\n📡 PING » {ping_time:.2f} ms\n\n<u>👨‍💻 Developer : @PythonBotz</u></b>", reply_markup = reply_markup,)
+
 
 
 @Bot.on_message(filters.private & filters.incoming)
