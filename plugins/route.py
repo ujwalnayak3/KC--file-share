557ea279336c4e2bae361f09d4cb89429b2e408e
Ben 10 <uncle@flash.co>
2025-03-06 13:45:48 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,5 +1,5 @@
-#(©)Codexbotz
-#rymme
+#(©) PythonBotz 
+#metaui
 
 
 
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    return web.json_response("PythonBotz")
