7269c6555b17a7d0e2124a80e9ecb7e10e40df17
Ben 10 <uncle@flash.co>
2025-03-06 16:29:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -26,7 +26,9 @@ FILE_AUTO_DELETE = TIME  # Example: 3600 seconds (1 hour)
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed1 & subscribed2 & subscribed3 & subscribed4)
 async def start_command(client: Client, message: Message):
+    await message.reply_chat_action(ChatAction.CHOOSE_STICKER)
     id = message.from_user.id
+ 
     if not await present_user(id):
         try:
             await add_user(id)
